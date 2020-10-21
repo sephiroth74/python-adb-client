8d6c15e2b12ffdd540ecb22e7f4eaa019c5dfcbf
Alessandro Crugnola <alessandro.crugnola@gmail.com>
2020-10-21 18:20:25 +0200

    requirements

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import sys
+import re
 from setuptools import find_namespace_packages, setup
 
 assert sys.version_info >= (3, 8, 0), "STB Utilities requires Python 3.8+"
@@ -6,13 +7,18 @@ assert sys.version_info >= (3, 8, 0), "STB Utilities requires Python 3.8+"
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open("src/__version__.py") as fh:
-    version = fh.read()
+VERSIONFILE = "src/adb/_version.py"
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
 
 setup(
     name="adb-client",
-    version=version,
-    scripts=["src/adb"],
+    version=verstr,
     author="Alessandro Crugnola",
     author_email="alessandro.crugnola@gmail.com",
     description="pure python adb client for mac/win/linux",
@@ -23,7 +29,13 @@ setup(
     py_modules=[],
     package_dir={"": "src"},
     python_requires=">=3.8",
-    packages=find_namespace_packages(where="src"),
+    packages=find_namespace_packages(where="src", exclude=("tests", "htmlconv")),
+    test_suite='nose.collector',
+    install_requires=[
+        "verboselogs>=1.7",
+        "zope.event>=4.5.0",
+        "coloredlogs>=14.0",
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
--- src/adb/__init__.py
+++ src/adb/__init__.py
@@ -2,3 +2,6 @@ from . import adb_connection
 from .adb_client import ADBClient
 from .adb_device import ADBDevice
 from .keycodes import KeyCodes, CPlusPlusKeyCodes
+from ._version import __version__
+
+__author__ = 'Alessandro Crugnola'
